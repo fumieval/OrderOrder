66141443e70e9c4d924e325e1f4148eb7e057e68
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2022-04-11 10:39:14 +0900

    Support GHC 8.10

--- src/OrderOrder.hs
+++ src/OrderOrder.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE LambdaCase #-}
 module OrderOrder (plugin) where
 
 import Control.Monad
+import Control.Monad.IO.Class
 import Data.Maybe (isNothing)
 import Data.Map.Strict qualified as M
 import Data.Set qualified as Set
@@ -11,20 +13,37 @@ import Data.List (intercalate)
 import Data.List.Split (splitOn)
 import Data.Foldable
 import Data.Yaml qualified as Yaml
+#if MIN_VERSION_ghc(9,0,0)
 import GHC.Data.Bag (listToBag)
 import GHC.Hs (hsmodImports, ImportDecl(..), HsParsedModule(..), HsModule(..), SrcSpanAnn'(..))
 import GHC.Plugins hiding ((<>))
 import GHC.Types.Error
 import GHC.Tc.Utils.Monad (getTopEnv, tcg_mod)
 import GHC.Unit.Module.Graph
+#else
+import Bag (listToBag)
+import DynFlags
+import ErrUtils (mkPlainErrMsg)
+import GHC.Hs (HsModule(..), ideclName)
+import HscTypes
+import Module (ModuleName, moduleName, pprModuleName, moduleNameString)
+import Outputable ((<+>))
+import Plugins
+import SrcLoc (SrcSpan, GenLocated(..))
+import TcRnMonad
+#endif
 import System.Directory
 import PrefixMap qualified as PM
 
 moduleKey :: ModuleName -> [String]
 moduleKey = splitOn "." . moduleNameString
-
+#if MIN_VERSION_ghc(9,0,0)
 listImports :: HsModule -> [(SrcSpan, ModuleName, ModuleName)]
 listImports hsmod = [(locA loc, this, that)
+#else
+listImports :: HsModule pass -> [(SrcSpan, ModuleName, ModuleName)]
+listImports hsmod = [(loc, this, that)
+#endif
   | L _ this <- maybe [] pure $ hsmodName hsmod
   , L loc that' <- hsmodImports hsmod
   , let L _ that = ideclName that'
@@ -49,10 +68,18 @@ verify pm rawMapping = do
           $ PM.lookup (moduleKey this) mapping
           >>= PM.lookup (moduleKey that)
         ]
+#if MIN_VERSION_ghc(9,0,0)
   unless (null problems) $ throwErrors $ listToBag
     [ mkPlainMsgEnvelope loc
     $ pprModuleName this <+> "imports" <+> pprModuleName that
     | (loc, this, that) <- problems]
+#else
+  dynFlags <- getDynFlags
+  unless (null problems) $ throwErrors $ listToBag
+    [ mkPlainErrMsg dynFlags loc
+    $ pprModuleName this <+> "imports" <+> pprModuleName that
+    | (loc, this, that) <- problems]
+#endif
 
 plugin :: Plugin
 plugin = defaultPlugin
@@ -65,8 +92,7 @@ plugin = defaultPlugin
   , pluginRecompile = flagRecompile
   , renamedResultAction = \_args tcGblEnv ghcRn -> do
     env <- getTopEnv
-    dynFlags <- getDynFlags
-    when (moduleName (tcg_mod tcGblEnv) == mainModuleNameIs dynFlags)
+    when (moduleNameString (moduleName (tcg_mod tcGblEnv)) == "Main")
       $ liftIO
       $ Yaml.encodeFile summaryFileName
       $ summarise $ hsc_mod_graph env
