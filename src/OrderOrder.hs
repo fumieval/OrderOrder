78d9fbe0fdd843394a0aa1b450b93b74dc0eb35e
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2022-04-29 19:53:46 +0900

    generate topologically sorted lists per prefix

--- src/OrderOrder.hs
+++ src/OrderOrder.hs
@@ -6,112 +6,83 @@ module OrderOrder (plugin) where
 
 import Control.Monad
 import Control.Monad.IO.Class
-import Data.Maybe (isNothing)
+import Data.Graph qualified as G
+import Data.List (intercalate, stripPrefix)
+import Data.List.Split (splitOn)
 import Data.Map.Strict qualified as M
 import Data.Set qualified as Set
-import Data.List (intercalate)
-import Data.List.Split (splitOn)
-import Data.Foldable
 import Data.Yaml qualified as Yaml
+import Text.Dot qualified as Dot
+
 #if MIN_VERSION_ghc(9,0,0)
-import GHC.Data.Bag (listToBag)
-import GHC.Hs (hsmodImports, ImportDecl(..), HsParsedModule(..), HsModule(..), SrcSpanAnn'(..))
 import GHC.Plugins hiding ((<>))
-import GHC.Types.Error
-import GHC.Tc.Utils.Monad (getTopEnv, tcg_mod)
+import GHC.Tc.Utils.Monad (getTopEnv)
 import GHC.Unit.Module.Graph
 #else
-import Bag (listToBag)
 import DynFlags
-import ErrUtils (mkPlainErrMsg)
 import GHC.Hs (HsModule(..), ideclName)
 import HscTypes
 import Module (ModuleName, moduleName, pprModuleName, moduleNameString)
-import Outputable ((<+>))
+import Outputable ((<+>), ppr, printForUser, alwaysQualify, defaultUserStyle)
 import Plugins
 import SrcLoc (SrcSpan, GenLocated(..))
 import TcRnMonad
 #endif
-import System.Directory
-import PrefixMap qualified as PM
 
 moduleKey :: ModuleName -> [String]
 moduleKey = splitOn "." . moduleNameString
-#if MIN_VERSION_ghc(9,0,0)
-listImports :: HsModule -> [(SrcSpan, ModuleName, ModuleName)]
-listImports hsmod = [(locA loc, this, that)
-#else
-listImports :: HsModule pass -> [(SrcSpan, ModuleName, ModuleName)]
-listImports hsmod = [(loc, this, that)
-#endif
-  | L _ this <- maybe [] pure $ hsmodName hsmod
-  , L loc that' <- hsmodImports hsmod
-  , let L _ that = ideclName that'
-  ]
-
-summaryFileName :: FilePath
-summaryFileName = "import-summary.yaml"
-
-verify :: HsParsedModule -> Summary -> Hsc ()
-verify pm rawMapping = do
-  let mapping = mconcat
-        [ PM.singleton (splitOn "." k) $ PM.singleton (splitOn "." v) ()
-        | (k, vs) <- M.toList rawMapping
-        , v <- vs
-        ]
-  let L _ hsmod = hpm_module pm
-  let problems = [(loc, this, that)
-        | (loc, this, that) <- listImports hsmod
-        , m <- toList $ PM.lookup (moduleKey that) mapping
-        , () <- toList $ PM.lookup (moduleKey this) m
-        , isNothing
-          $ PM.lookup (moduleKey this) mapping
-          >>= PM.lookup (moduleKey that)
-        ]
-#if MIN_VERSION_ghc(9,0,0)
-  unless (null problems) $ throwErrors $ listToBag
-    [ mkPlainMsgEnvelope loc
-    $ pprModuleName this <+> "imports" <+> pprModuleName that
-    | (loc, this, that) <- problems]
-#else
-  dynFlags <- getDynFlags
-  unless (null problems) $ throwErrors $ listToBag
-    [ mkPlainErrMsg dynFlags loc
-    $ pprModuleName this <+> "imports" <+> pprModuleName that
-    | (loc, this, that) <- problems]
-#endif
 
 plugin :: Plugin
 plugin = defaultPlugin
-  { parsedResultAction = \_args _ pm -> do
-    exist <- liftIO $ doesFileExist summaryFileName
-    if exist
-      then Yaml.decodeFileThrow summaryFileName >>= verify pm
-      else pure ()
-    pure pm
+  { parsedResultAction = \_args _ pm -> pure pm
   , pluginRecompile = flagRecompile
-  , renamedResultAction = \_args tcGblEnv ghcRn -> do
+  , typeCheckResultAction = \args _ tcGblEnv -> do
     env <- getTopEnv
-    when (moduleNameString (moduleName (tcg_mod tcGblEnv)) == "Main")
-      $ liftIO
-      $ Yaml.encodeFile summaryFileName
-      $ summarise $ hsc_mod_graph env
-
-    pure (tcGblEnv, ghcRn)
+    case args of
+      [stripPrefix "export:" -> Just path] -> do
+        let summary = summarise $ hsc_mod_graph env
+        liftIO $ Yaml.encodeFile path $ sortGroup <$> summary
+        liftIO $ writeFile (path <> ".dot") $ toDot summary
+      [] -> pure ()
+      _ -> error "Usage: -fplugin-opt=OrderOrder:export:/path/to"
+    pure tcGblEnv
   }
 
-type Summary = M.Map String [String]
+type Group = M.Map String (Set.Set String)
+type Summary = M.Map String Group
+
+relations :: [String] -> [String] -> [(String, String, [String])]
+relations [] _ = []
+relations _ [] = []
+relations (x : xs) (y : ys)
+  | x == y = fmap (x:) <$> relations xs ys
+  | otherwise = [(x, y, [])]
+
+sortGroup :: Group -> [String]
+sortGroup group = [ k | (k, _, _) <- map nodeFromVertex $ G.reverseTopSort graph ] where
+  (graph, nodeFromVertex, _vertexFromKey) = G.graphFromEdges
+    [ (k, k, Set.toList vs) | (k, vs) <- M.toList group]
 
 summarise :: ModuleGraph -> Summary
-summarise graph = M.fromList
-  $ map (\(k, v) -> (intercalate "." k, [ intercalate "." j | (j, _) <- PM.toList v]))
-  $ PM.toList
-  $ mconcat
-    [ PM.singleton k $ PM.singleton (moduleKey m) ()
+summarise graph = M.fromListWith (M.unionWith (<>))
+    [ el
     | ms <- mgModSummaries graph
-    , let k = moduleKey $ moduleName $ ms_mod ms
+    , let src = moduleKey $ moduleName $ ms_mod ms
     , (_, L _ m) <- ms_textual_imps ms
-    , m `Set.member` ourModules
+    , let dst = moduleKey m
+    , dst `Set.member` ourModules
+    , (k, d, p) <- relations src dst
+    , let prefix = intercalate "." p
+    , el <- [(prefix, M.singleton k $ Set.singleton d), (prefix, M.singleton d mempty)]
     ]
   where
-    ourModules = Set.fromList $ moduleName . ms_mod <$> mgModSummaries graph
+    ourModules = Set.fromList $ moduleKey . moduleName . ms_mod <$> mgModSummaries graph
+
+toDot :: Summary -> String
+toDot summary = Dot.showDot $ do
+  Dot.attribute ("rankdir", "LR")
+  forM_ (M.toList summary) $ \(prefix, group) -> Dot.scope $ do
+    Dot.attribute ("label", prefix)
+    nodes <- M.traverseWithKey (\k vs -> (,) vs <$> Dot.node [("label", k)]) group
+    forM_ (M.elems nodes) $ \(vs, node) -> do
+      forM_ vs $ \v -> forM_ (M.lookup v nodes) $ \(_, n') -> node Dot..->. n'
