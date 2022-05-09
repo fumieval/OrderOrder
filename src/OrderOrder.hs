2ed494c2225fb2392ca28ec1f7715ad41f2444bc
Fumiaki Kinoshita <fumiaki.kinoshita@herp.co.jp>
2022-05-09 10:43:54 +0900

    find FAS

--- src/OrderOrder.hs
+++ src/OrderOrder.hs
@@ -7,9 +7,11 @@ module OrderOrder (plugin) where
 import Control.Monad
 import Control.Monad.IO.Class
 import Data.Graph qualified as G
-import Data.List (intercalate, stripPrefix)
+import Data.Foldable
+import Data.List (intercalate, stripPrefix, sortBy)
 import Data.List.Split (splitOn)
 import Data.Map.Strict qualified as M
+import Data.Ord (comparing)
 import Data.Set qualified as Set
 import Data.Yaml qualified as Yaml
 import Text.Dot qualified as Dot
@@ -35,10 +37,7 @@ plugin = defaultPlugin
     case args of
       [stripPrefix "export:" -> Just path] -> liftIO $ do
         let raw = dependencies $ hsc_mod_graph env
-        let summary = summarise raw
-        let sorted = sortGroup <$> summary
-        Yaml.encodeFile (path <> ".yaml") sorted
-        writeFile (path <> ".dot") $ toDot summary
+        runOrderOrder path raw
       [] -> pure ()
       _ -> error "Usage: -fplugin-opt=OrderOrder:export:/path/to"
     pure tcGblEnv
@@ -47,17 +46,20 @@ plugin = defaultPlugin
 type Graph = M.Map String (M.Map String Int)
 type Summary = M.Map String Graph
 
+runOrderOrder :: FilePath -> Graph -> IO ()
+runOrderOrder path raw = do
+  let summary = summarise raw
+  Yaml.encodeFile (path <> ".yaml") summary
+  writeFile (path <> ".dot") $ toDot summary
+  writeFile (path <> ".trims.txt")
+    $ unlines $ suggestTrims raw $ findFAS <$> summary
+
 relations :: [String] -> [String] -> [(String, String, [String])]
 relations [] _ = []
 relations _ [] = []
 relations (x : xs) (y : ys)
   | x == y = fmap (x:) <$> relations xs ys
   | otherwise = [(x, y, [])]
-
-sortGroup :: Graph -> [String]
-sortGroup group = [ k | (k, _, _) <- map nodeFromVertex $ G.reverseTopSort graph ] where
-  (graph, nodeFromVertex, _vertexFromKey) = G.graphFromEdges
-    [ (k, k, M.keys vs) | (k, vs) <- M.toList group]
 
 dependencies :: ModuleGraph -> Graph
 dependencies graph = M.fromListWith (<>)
@@ -80,12 +82,47 @@ summarise graph = M.fromListWith (M.unionWith (M.unionWith (+)))
   , el <- [(prefix, M.singleton k $ M.singleton d weight), (prefix, M.singleton d mempty)]
   ]
 
+type Connectivity a = M.Map a (Set.Set a)
+
+isConnected :: Ord a => Connectivity a -> a -> a -> Bool
+isConnected conn src dst = case M.lookup src conn of
+  Just set -> Set.member dst set || any (\x -> isConnected conn x dst) set
+  Nothing -> False
+
+insertConn :: Ord a => a -> a -> Connectivity a -> Connectivity a
+insertConn src dst = M.insertWith (<>) src (Set.singleton dst)
+
+-- | Find a feedback arc set (a set of edges that would eliminate SCCs from the given graph)
+findFAS :: Graph -> Graph
+findFAS graph = snd $ foldl' step (mempty, mempty) allEdges
+  where
+    step (conn, acc) (src, dst)
+      | isConnected conn dst src = (conn, M.insertWith (<>) src (M.singleton dst 1) acc)
+      | otherwise = (insertConn src dst conn, acc)
+    allEdges = map fst $ sortBy (flip $ comparing snd) $
+      [ ((src, dst), weight)
+      | (src, dsts) <- M.toList graph
+      , (dst, weight) <- M.toList dsts
+      ]
+
+-- Instantiate concrete module names from the given FAS
+suggestTrims :: Graph -> M.Map String Graph -> [String]
+suggestTrims rawGraph trimMap =
+  [ unwords [src, "->", dst]
+  | (src, dsts) <- M.toList rawGraph
+  , dst <- M.keys dsts
+  , (k, d, prefix) <- relations (splitOn "." src) (splitOn "." dst)
+  , trims <- toList $ M.lookup (intercalate "." prefix) trimMap
+  , ds <- toList $ M.lookup k trims
+  , M.member d ds
+  ]
+
 cluster :: Graph -> Dot.Dot ()
 cluster group = do
   nodes <- M.traverseWithKey (\k vs -> (,) vs <$> Dot.node [("label", k)]) group
   forM_ (M.elems nodes) $ \(vs, node) -> do
     forM_ (M.toList vs) $ \(v, weight) -> forM_ (M.lookup v nodes)
-      $ \(_, n') -> Dot.edge node n' [("weight", show weight)]
+      $ \(_, n') -> Dot.edge node n' [("weight", show weight), ("label", show weight)]
 
 toDot :: Summary -> String
 toDot summary = Dot.showDot $ do
